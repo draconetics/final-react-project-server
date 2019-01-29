d70310188ba0516a7e6fd0a01e6621374a0738bc
Mario Flores <dracon@hotmail.es>
2019-01-29 17:05:12 -0400

    crud of item and comment

--- src/routes/galery.js
+++ src/routes/galery.js
@@ -13,20 +13,7 @@ router.get('/items', (req, res) => {
     });
 });
 
-/*
-router.get('/items', (req, res) => {
-    const {id, name, image, category, label, price, featured, description} = req.body;
-    const query = ' INSERT INTO items VALUES(?, ?, ?, ?, ?, ?, ?, ?);';
-    mysqlConnection.query(query, [id, name, image, category, label, price, featured, description], (err, rows, fields)=>{
-        
-        if(!err){
-            res.json({id: rows.insertId, title, rating});
-        }else{
-            console.log(err);
-        }
-    });
-});
-*/
+
 router.get('/items/:id',(req, res) =>{
     const {id} = req.params;
     console.log(id);
@@ -39,5 +26,102 @@ router.get('/items/:id',(req, res) =>{
     });
 });
 
+//procedimiento almacenado.
+router.post('/items', (req, res)=>{
+    const { name, image, category, label, price, featured, description} = req.body;
+    const query = ' INSERT INTO items (name, image, category, label, price, featured, description) VALUES( ?, ?, ?, ?, ?, ?, ?);';
+    mysqlConnection.query(query, [ name, image, category, label, price, featured, description], (err, rows, fields)=>{
+        
+        if(!err){
+            res.json({id: rows.insertId, name, image, category, label, price, featured, description});
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.put('/items/:id',(req, res)=>{
+    const { name, image, category, label, price, featured, description} = req.body;
+    const {id} = req.params;
+    const query = ' UPDATE items set name=?, image=?, category=?, label=?, price=?, featured=?, description=? WHERE id=?;';
+    mysqlConnection.query(query, [ name, image, category, label, price, featured, description, id], (err, rows, fields)=>{
+        if(!err){
+            res.json({status: "Item updated"});
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.delete('/items/:id', (req, res)=>{
+    const {id} = req.params;
+    mysqlConnection.query('DELETE FROM items WHERE id=?', [id],(err, rows, fields)=>{
+        if(!err){
+            res.json({status: "Item deleted!!"});
+        }else{
+            console.log("error!!!");
+        }
+    });
+});
+/********************************************************** */
+
+router.get('/comments', (req, res) => {
+    mysqlConnection.query('SELECT * FROM comments', (err, rows, fields)=>{
+        if(!err){
+            res.json(rows);
+        }else {
+            console.log(err);
+        }
+    });
+});
+
+router.get('/comments/:id',(req, res) =>{
+    const {id} = req.params;
+    console.log(id);
+    mysqlConnection.query('SELECT * FROM comments WHERE id=?',[id],(err, rows, fields)=>{
+        if(!err){
+            res.json(rows[0]);
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.post('/comments', (req, res)=>{
+    const { rating, comment, author, date, itemId} = req.body;
+    const query = ' INSERT INTO comments (rating, comment, author, date, itemId) VALUES( ?, ?, ?, ?, ?);';
+    mysqlConnection.query(query, [ rating, comment, author, date, itemId], (err, rows, fields)=>{
+        
+        if(!err){
+            res.json({id: rows.insertId, rating, comment, author, date, itemId});
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.put('/comments/:id',(req, res)=>{
+    const { rating, comment, author, date, itemId} = req.body;
+    const {id} = req.params;
+    const query = ' UPDATE comments set rating=?, comment=?, author=?, date=?, itemId=? WHERE id=?;';
+    mysqlConnection.query(query, [ rating, comment, author, date, itemId, id], (err, rows, fields)=>{
+        if(!err){
+            res.json({status: "Comment updated"});
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.delete('/comments/:id', (req, res)=>{
+    const {id} = req.params;
+    mysqlConnection.query('DELETE FROM comments WHERE id=?', [id],(err, rows, fields)=>{
+        if(!err){
+            res.json({status: "Comment deleted!!"});
+        }else{
+            console.log("error!!!");
+        }
+    });
+});
 
 module.exports = router;
